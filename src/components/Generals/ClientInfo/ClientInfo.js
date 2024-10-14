c6aeec027d2b1785a0258fc57b9f77f5b3410766
PabloLz-01 <pablolz190402@gmail.com>
2024-10-14 09:14:59 -0600

    changes

--- src/components/Generals/ClientInfo/ClientInfo.js
+++ src/components/Generals/ClientInfo/ClientInfo.js
@@ -22,54 +22,18 @@ const { Text } = Typography;
 export default function ClientInfo(props) {
   const { reload, setReload, profile, role } = props;
   const desc = ["Horrible", "Malo", "Normal", "Bueno", "Excelente"];
-  const types = ["image/jpeg", "image/jpg", "image/png"];
 
-  let token = localStorage.getItem("token");
+  const [form] = Form.useForm();
 
-  const propsFile = {
-    name: "img",
-    multiple: false,
-    data: {
-      org_id: profile.organization_id,
-    },
-    action: "https://api.epno-app.com/api/change_profile_image",
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    onChange(info) {
-      const { status } = info.file;
-      if (status === "done") {
-        if (info.file.response.success == true) {
-          setReload(!reload);
-          message.success(
-            `${info.file.name}, ${info.file.response.message}.`,
-            10
-          );
-        } else {
-          message.error(
-            `Hubo un error al subir el archivo ${info.file.name}, ${info.file.response.message}.`,
-            10
-          );
-        }
-      }
-    },
-    beforeUpload: (file) => {
-      if (!types.includes(file.type)) {
-        message.error(
-          `El archivo ${file.name}, esta en un formato no permitido, 
-           toma en cuenta que no sera subido`,
-          10
-        );
-        return false;
-      } else {
-        return true;
-      }
-    },
-    onDrop(e) {
-      console.log("Dropped files", e.dataTransfer.files);
-    },
-  };
+  const [editProfile, setEditProfile] = useState(false)
+  const [editImg, setEditImg] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  console.log(props)
+
+  const onFinish = (values) => {
+    console.log(values)
+  }
 
   return (
     <>
@@ -77,7 +41,7 @@ export default function ClientInfo(props) {
         <Skeleton active avatar />
       ) : (
         <Row gutter={[12, 12]}>
-          <Col xs={24} md={12}>
+          <Col xs={24} md={8}>
             <Row gutter={[12, 12]}>
               <Col xs={0} sm={24} style={{ alignItems: "center" }}>
                 <Avatar
@@ -86,9 +50,8 @@ export default function ClientInfo(props) {
                   shape="square"
                 />
               </Col>
-              {/*
-              <Col sm={24} style={{ alignItems: "center" }}>
-                <Upload {...propsFile} maxCount={1} accept=".png,.jpg,.jpeg">
+              <Col sm={12} style={{ alignItems: "center" }}>
+                <Upload  maxCount={1} accept=".png,.jpg,.jpeg">
                   <Button icon={<UploadOutlined />}>
                     <Tooltip title="Acepta png,jpg y jpeg">
                       Cambiar foto
@@ -96,13 +59,18 @@ export default function ClientInfo(props) {
                   </Button>
                 </Upload>
               </Col>
-               */}
-              
+              <Col sm={12} style={{ alignItems: "center" }}>   
+                <Button color="primary" onClick={() => {}}>
+                  Cambiar foto de perfil
+                </Button>
+              </Col>
             </Row>
           </Col>
-          <Col xs={24} md={12}>
+          <Col xs={24} md={16}>
+          {!editProfile ? 
             <Row gutter={[12, 12]}>
               <Col xs={24} md={10}>
+              
                 <Text>
                   Tel:<Title level={4}>{profile.phone}</Title>
                 </Text>
@@ -158,8 +126,88 @@ export default function ClientInfo(props) {
                 )}
               </Col>
             </Row>
+          :
+          <Form name='update_profile' className='profile-form' onFinish={onFinish} form={form} layout="vertical">
+            <Row gutter={[12, 12]}>
+              <Col xs={24}>
+                <Form.Item name='name' label='Nombre completo'>
+                  <Input  placeholder={profile.name} value={profile.name}  />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item name='phone' label='Número telefónico'>
+                  <Input  placeholder={profile.phone} value={profile.phone}  />
+                </Form.Item>
+              </Col>
+
+              {/* UBICACION ---------------------------------------- */}
+              <Col xs={24} md={4}>
+                <Form.Item name='country' label='País'>
+                  <Input  placeholder={profile.country} value={profile.country}  />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={6}>
+                <Form.Item name='state' label='Estado'>
+                  <Input  placeholder={profile.state} value={profile.state}  />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={8}>
+                <Form.Item name='city' label='Ciudad'>
+                  <Input  placeholder={profile.city} value={profile.city}  />
+                </Form.Item>
+              </Col>
+
+              {/* DIRECCION */}
+              <Col xs={24} md={8}>
+                <Form.Item name='city' label='Ciudad'>
+                  <Input  placeholder={profile.city} value={profile.city}  />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={8}>
+                <Form.Item name='city' label='Ciudad'>
+                  <Input  placeholder={profile.city} value={profile.city}  />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={8}>
+                <Form.Item name='city' label='Ciudad'>
+                  <Input  placeholder={profile.city} value={profile.city}  />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+              <Form.Item shouldUpdate={true}>
+                {() => (
+                  <Button
+                    style={{ width: '100%' }}
+                    type='primary'
+                    htmlType='submit'
+                    className='login-form-button'
+                    loading={loading}
+                  >
+                    Guardar cambios
+                  </Button>
+                )}
+              </Form.Item>
+              </Col>
+            </Row>
+              
+              
+
+            </Form>
+          }
+            
           </Col>
+          {/* 
+          <Col xs={24} md={12}>
+            <Row gutter={[12, 12]}>
+              <div>
+                <Button onClick={() => setEditProfile(!editProfile)}>Editar Perfil</Button>
+              </div>
+            </Row>
+          </Col>
+          */}
+          
         </Row>
+        
       )}
     </>
   );
